4b2954895fb05ff684cec7667a31f356c71749f7
Le Minh Tri <minhtri0795@gmail.com>
2023-09-20 10:40:46 +0700

    banner cookie

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -3,10 +3,20 @@ import FixedCta from "./FixedCTA";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Cookie from "./Cookie/Cookie";
+import Head from "next/head";
+import Script from "next/script";
 
 const Layout = ({ children }) => {
   return (
     <div className="relative">
+      <Head>
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="4c2f26c9-cd33-4e30-8da7-306b194e2b66"
+          data-blockingmode="auto"
+        />
+      </Head>
       <Header />
       <FixedCta />
       <main>{children}</main>
--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,16 +1,14 @@
 import Cookie from "@/components/Cookie/Cookie";
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
+import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="4c2f26c9-cd33-4e30-8da7-306b194e2b66" data-blockingmode="auto"></Script>
       <body>
         <Main />
         <NextScript />
       </body>
-       
     </Html>
   );
 }
