d02a36e08521dd3bfa061c5fecd675d733852e27
saigon.digital <phuoc@saigon.digital>
2023-09-28 17:24:29 +0700

    setup email client

--- frontend/src/pages/api/contact.js
+++ frontend/src/pages/api/contact.js
@@ -3,7 +3,6 @@ import Mailjet from "node-mailjet";
 import thankyouText from "../../../src/data/thankyou.json";
 
 export default async function handler(req, res) {
-  
   const publicKey = process.env.MAILJET_KEY_PUBLIC;
   const privateKey = process.env.MAILJET_KEY_PRIVATE;
 
@@ -20,8 +19,8 @@ export default async function handler(req, res) {
     Messages: [
       {
         From: {
-          Email: "developers@saigon.digital",
-          name: "Saigon Digital",
+          Email: "kathrin.risse@whw.de",
+          name: "WHW CD",
         },
         To: [
           {
@@ -30,19 +29,22 @@ export default async function handler(req, res) {
           },
         ],
         Subject: subject,
-        TextPart: "Thank you for getting in touch! We appreciate you contacting us. One of our colleagues will get back in touch with you soon!Have a great day!",
-        HTMLPart: thankyouText
+        TextPart:
+          "Thank you for getting in touch! We appreciate you contacting us. One of our colleagues will get back in touch with you soon!Have a great day!",
+        HTMLPart: thankyouText,
       },
     ],
   };
   try {
-    const request = await mailjet
-      .post("send", { version: "v3.1" })
-      .request(config);
+    const request = mailjet.post("send", { version: "v3.1" }).request(config);
 
-    return res
-      .status(request.response.status || 200)
-      .json({ success: true, data: request.data });
+    request
+      .then(result => {
+        return res.status(200).json({ success: true, data: result.body });
+      })
+      .catch(err => {
+        return res.status(500).json({ error: err.message });
+      });
   } catch (error) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
